153875a424966901e2c688ec65c4df2daec7607e
jinoyoko <retrogamer2112a@gmail.com>
2023-05-14 19:36:04 +0200

    Implemented mouse control:
    Added feature to control the view orientation with the mouse input.

--- CubenizerGame/src/Main.cpp
+++ CubenizerGame/src/Main.cpp
@@ -7,11 +7,19 @@
 
 using std::vector;
 
+double mouse_pos_x = 0.0, mouse_pos_y = 0.0;
+const double MouseSensitivity = 0.5;
+const Vector3F BackgroundColor(0.2f, 0.2f, 0.2f);
+
 void HandleInput(GLFWwindow* window, World &world_reference);
+void HandleMouseInput(GLFWwindow* window, World &world_reference);
+void HandleKeyboardInput(GLFWwindow* window, World &world_reference);
 void DrawScreen(RenderWindow *window_reference, World &world_reference);
 
 int main() {
     RenderWindow window("Test 3D-GL Window", 800, 600);
+    glfwGetCursorPos(window.getHandle(), &mouse_pos_x, &mouse_pos_y);
+
     RenderingSystem render_system;
     render_system.CreateShaders();
 
@@ -36,27 +44,54 @@ int main() {
 }
 
 void HandleInput(GLFWwindow* window, World &world_reference) {
+    HandleKeyboardInput(window, world_reference);
+    HandleMouseInput(window, world_reference);
+
+    world_reference.GetCamera().UpdateView();
+}
+
+void HandleKeyboardInput(GLFWwindow* window, World &world_reference) {
     if (glfwGetKey(window, GLFW_KEY_W))
-        world_reference.GetCamera().TranslateCamera(Vector3F(0.0, 0.0, -0.1));
+        world_reference.GetCamera().TranslateCamera(Vector3F(0.0, 0.0, -1.0));
 
     if (glfwGetKey(window,GLFW_KEY_S))
-        world_reference.GetCamera().TranslateCamera(Vector3F(0.0f, 0.0f, 0.1f));
+        world_reference.GetCamera().TranslateCamera(Vector3F(0.0, 0.0, 1.0));
 
     if (glfwGetKey(window,GLFW_KEY_A))
-        world_reference.GetCamera().TranslateCamera(Vector3F(-0.1f, 0.0f, 0.0f));
+        world_reference.GetCamera().TranslateCamera(Vector3F(-1.0, 0.0, 0.0));
 
     if (glfwGetKey(window,GLFW_KEY_D))
-        world_reference.GetCamera().TranslateCamera(Vector3F(0.1f, 0.0f, 0.0f));
+        world_reference.GetCamera().TranslateCamera(Vector3F(1.0, 0.0, 0.0));
 
     if (glfwGetKey(window,GLFW_KEY_LEFT_CONTROL))
-        world_reference.GetCamera().TranslateCamera(Vector3F(0.0f, -0.1f, 0.0f));
+        world_reference.GetCamera().TranslateCamera(Vector3F(0.0, -1.0, 0.0));
 
     if (glfwGetKey(window,GLFW_KEY_SPACE))
-        world_reference.GetCamera().TranslateCamera(Vector3F(0.0f, 0.1f, 0.0f));
+        world_reference.GetCamera().TranslateCamera(Vector3F(0.0, 1.0, 0.0));
+}
+
+void HandleMouseInput(GLFWwindow* window, World &world_reference) {
+    double current_mouse_pos_x = 0.0, current_mouse_pos_y = 0.0;
+    glfwGetCursorPos(window, &current_mouse_pos_x, &current_mouse_pos_y);
+
+    double delta_x = current_mouse_pos_x - mouse_pos_x;
+    double delta_y = current_mouse_pos_y - mouse_pos_y;
+
+    double move_angle_x = delta_x * MouseSensitivity;
+    double move_angle_y = delta_y * MouseSensitivity;
+
+    if (delta_x != 0.0)
+        world_reference.GetCamera().YawBy((float)move_angle_x);
+
+    if (delta_y != 0.0) {}
+        world_reference.GetCamera().PitchBy((float)move_angle_y);
+
+    mouse_pos_x = current_mouse_pos_x;
+    mouse_pos_y = current_mouse_pos_y;
 }
 
 void DrawScreen(RenderWindow *window_reference, World &world_reference) {
-    glClearColor ( 0.2f, 0.2f, 0.2f, 1.0f);
+    glClearColor (BackgroundColor.GetX(), BackgroundColor.GetY(), BackgroundColor.GetZ() , 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     world_reference.DrawWorld();
